85281dbb694d46c4e3009d303141c4ea17fc24aa
tomascavataio <tomascavataio@gmail.com>
2020-07-15 12:26:15 +0200

    Form validation, addService changes coming soon

--- src/app/containers/user-create/user-create.component.ts
+++ src/app/containers/user-create/user-create.component.ts
@@ -1,8 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnChanges, ViewChild, ElementRef } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { UserService } from 'src/app/services/user-service.service';
 import { Router } from '@angular/router';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
 import { ErrorStateMatcher } from '@angular/material/core';
 import { STEPPER_GLOBAL_OPTIONS } from '@angular/cdk/stepper';
 
@@ -11,28 +11,41 @@ import { STEPPER_GLOBAL_OPTIONS } from '@angular/cdk/stepper';
   templateUrl: './user-create.component.html',
   styleUrls: ['./user-create.component.scss'],
   providers: [{
-    provide: STEPPER_GLOBAL_OPTIONS, useValue: { showError: true }
+    provide: STEPPER_GLOBAL_OPTIONS, useValue: { showError: true, displayDefaultIndicatorType: false }
   }]
 })
 export class UserCreateComponent implements OnInit {
   userForm: FormGroup;
-  stepFormGroup: FormGroup;
-  depends: boolean;
+  medicalForm: FormGroup;
+  isPacient: boolean;
+  pacient: string;
+
   constructor(private http: HttpClient, private userService: UserService, private router: Router, private formBuilder: FormBuilder) {
-    this.createForm();
+
   }
 
   ngOnInit(): void {
+    this.createForm();
+    this.isPacient = true;
+    this.userForm.controls.userType.setValue('pacient');
   }
 
   createForm(): void {
-    this.stepFormGroup = this.formBuilder.group({
-      name: ['', Validators.required]
-    });
     this.userForm = this.formBuilder.group({
       name: ['', Validators.required],
-      surname: ['', Validators.required]
+      surname: ['', Validators.required],
+      userType: ['', Validators.required]
     });
+    this.medicalForm = this.formBuilder.group({
+      nhc: ['', Validators.nullValidator],
+      medicalBoard: ['', Validators.nullValidator]
+    });
+  }
+
+  verifyUser(): void {
+    if (this.isPacient === true) {
+      this.isPacient = false;
+    } else { this.isPacient = true; }
   }
 
   // tslint:disable-next-line: max-line-length
